a4d0ca9bdb81fe745dfed1daab0ca8365d76cb8a
Ana Luiza <104218495+luucaps@users.noreply.github.com>
2024-10-14 20:22:44 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# Entrega Sprint 1 - Programa de Bolsas Compass UOL / AWS - turma setembro/2024
+
+Entrega da atividade da primeira sprint do programa de bolsas Compass UOL para formação em Inteligência Artificial para AWS.
+
+***
+
+## Descrição do projeto
+
+O projeto foi desenvolvido para o programa de bolsas da Compass UOL. 
+
+A atividade consiste em um sistema de armazenamento de dados cadastrais feito em javascript, em que possui as funcionalidades de cadastrar, consultar e deletar usuário. Os dados para cadastramento de usuários são: nome, data de nascimento, telefone e e-mail, e foi utilizado o localStorage para armazenar esses dados.
+
+***
+
+## Funcionalidades do projeto
+
+- `Funcionalidade 1`: Realizar cadastro dos usuários.
+- `Funcionalidade 2`: Armazenar dados do usuário, como nome, data de nascimento, telefone e e-mail no localStorage.
+- `Funcionalidade 3`: Consultar usuário e verificar seus dados.
+- `Funcionalidade 4`: Deletar um usuário.
+
+***
+
+## Ferramentas utilizadas
+
+- Javascript
+- VSCode
+
+
+***
+
+## Acesso ao projeto
+
+Você pode acessar o [código fonte do projeto](https://github.com/Compass-pb-aws-2024-SETEMBRO/sprint-1-pb-aws-setembro/tree/ana-capistrano) ou baixá-lo.
+
+***
+
+## Dificuldades enfrentadas
+
+* Dificuldade em realizar toda a lógica do arquivo.js;
+* Dificuldade com o padrão Factory;
+* Dificuldade com JavaScript
+
+***
+
+## Autoria do projeto
+
+Desenvolvido por [Ana Luiza Capistrano dos Santos](https://github.com/luucaps)
---
+++ arquivo.js
@@ -0,0 +1,128 @@
+'use strict'
+
+//Abrir tela de cadastro
+const abrirTela = () => document.getElementById('tela_cadastro_js').classList.add('active')
+//Fechar tela de cadastro
+const closeTela = () => {
+    limparCampos()
+    document.getElementById('tela_cadastro_js').classList.remove('active')
+}
+
+//localStorage
+const getLocalStorage = () =>  JSON.parse(localStorage.getItem('db_usur')) ?? []
+const setLocarStorage = (db_usur) => localStorage.setItem("db_usur", JSON.stringify(db_usur))
+
+//DELETE
+const deleteUsuario = (indexUsuario) => {
+    const db_usur = readUsuario() 
+    db_usur.splice(indexUsuario, 1)
+    setLocarStorage(db_usur)
+}
+
+//READ
+const readUsuario = () => getLocalStorage()
+
+//CREATE
+const createUsuario = (usuario) => {
+    const db_usur = getLocalStorage()
+    db_usur.push (usuario)
+    setLocarStorage(db_usur)
+}
+
+//Validar os campos de cadastro
+const campoValido = () => {
+    return document.getElementById('tela_cadastro_form').reportValidity()
+}
+
+const limparCampos = () => {
+    const campos = document.querySelectorAll('.campo_cadastro')
+    campos.forEach(campo => campo.value = "")
+}
+
+//Interação com o html
+const saveUsuario = () =>{
+    if (campoValido()){
+        const usuario = {
+            nome     : document.getElementById('nome_usuario').value,
+            dataNasc : document.getElementById('nasc_usuario').value,
+            telefone : document.getElementById('tel_usuario').value,
+            email    : document.getElementById('email_usuario').value
+        }
+        createUsuario(usuario)
+        console.log("Usuário cadastrado.")
+        limparCampos()
+        updateTabela()
+        closeTela()
+    } 
+}
+
+//Criar linhas na tabela
+const criarLinha = (usuario, indexUsuario) =>{
+    const newLinha = document.createElement('tr')
+    newLinha.innerHTML = `
+        <td>${usuario.nome}</td>
+        <td>${usuario.dataNasc}</td>
+        <td>${usuario.telefone}</td>
+        <td>${usuario.email}</td>
+        <td>
+            <button type="button" class="botao_consultar" id="consultar-${indexUsuario}">Consultar</button>
+            <button type="button" class="botao_deletar" id="deletar-${indexUsuario}">Deletar</button>
+        </td>
+    `
+    document.querySelector('#tabela_user>tbody').appendChild(newLinha)
+}
+
+//Limpar a tabela antes de acontecer o update/atualização
+const limparTabela = () => {
+    const rows = document.querySelectorAll('#tabela_user>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+//Atualizar tabela
+const updateTabela = () => {
+    const db_usur = readUsuario()
+    limparTabela()
+    db_usur.forEach(criarLinha)
+}
+
+//Pegar info do usuario
+const viewUsuario = (usuario) => {
+    document.getElementById('nome_usuario').value = usuario.nome
+    document.getElementById('nasc_usuario').value = usuario.dataNasc
+    document.getElementById('tel_usuario').value = usuario.telefone
+    document.getElementById('email_usuario').value = usuario.email
+}
+
+//Consultar usuário
+const consultUsuario = (index) => {
+    const usuario = readUsuario()[index]
+    viewUsuario(usuario)
+    console.log("Consultando usuário.")
+    abrirTela()
+}
+
+//Consultar e deletar
+const consultarDeletar = (evento) => {
+    if (evento.target.type == 'button'){
+        const [action, index] = evento.target.id.split('-')
+        if (action == 'consultar'){
+            consultUsuario(index)
+        }else{
+            const usuario = readUsuario()[index]
+            const response = confirm (`Deseja realmente deletar o usuário ${usuario.nome}?`)
+            if (response){
+                deleteUsuario(index)
+                console.log("Usuário deletado.")
+                updateTabela()
+            }
+        }
+    }
+}
+
+updateTabela()
+
+//Ações de click
+document.getElementById('cadastrar_usuario').addEventListener('click', abrirTela)
+document.getElementById('tela_cadastro_close_js').addEventListener('click', closeTela)
+document.getElementById('botao_salvar_usuario').addEventListener('click', saveUsuario)
+document.querySelector('#tabela_user>tbody').addEventListener('click', consultarDeletar)
