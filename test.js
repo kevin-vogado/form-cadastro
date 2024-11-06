24571405dd1d01d871333e0b2ac5d4c7510d3fd9
PedroMoraes9009 <pedro.h.mm9@gmail.com>
2024-11-05 21:25:17 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 QUnit.test("Primeiro teste com QUnit", function (assert) {
-    // Aqui, verificamos se a função retorna true
+    // Aqui, verificamos se a função retorna false
     assert.ok(saveUsuario() === false, "Passou!");
 });
